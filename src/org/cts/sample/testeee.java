da9c237f035eee535c0d5c1d6d6c4d44932d552a
jeyachitrachithu <jeyasa5191@gmail.com>
2019-09-19 12:16:48 +0530

    fixing my code

---
+++ src/org/cts/sample/testeee.java
@@ -0,0 +1,18 @@
+package org.cts.sample;
+
+public class testeee {
+	public void testeeeName() {
+		System.out.println("chithu");
+		}
+	public void testeeeid () {
+		System.out.println("124");
+		
+	}
+	public static void main(String[] args) {
+	testeee r =new testeee ();
+	r.testeeeid();
+	r.testeeeName();
+		
+	}
+}
+
